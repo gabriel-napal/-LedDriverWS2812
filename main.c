fcd7725a2b3a1a9c57444d4ee6887783ce172d8d
gabriel-napal <gabrielnapalhm@gmail.com>
2020-04-09 00:29:27 +0200

    Clean up, added timers library and modified main loop to be non-blocking

--- main.c
+++ main.c
@@ -13,6 +13,24 @@
  *   3V3            5VDC (it works with 3V3)s
  *   P2.0(MOSI)     DIN
  *   GND            GND
+ *
+ *   Program Flow :
+ *
+ * 1) Initialization of clocks, SPI, LED Table and Timer
+ * 2) Timer starts
+ * 3) Sends First Frame
+ * 4) Next LED composition is calculated (but not sent)
+ * 5) CPU goes to LPM (Low Power Mode), nothing is executed.
+ * 6) When the TimerA0 "ticks", CPU is woke up, the main execution loop proceeds (Back to step 3)
+ *
+ *
+ *  General configuration :
+ *
+ *  CPU : working at 4 Mhz
+ *  SPI : CLK is set to 6 MHz
+ *  TimerA0 : uses a 8.42kHz clock. Its internal counter can be used to increase or reduce the main loop frequency
+ *          Since we're using LPM and Timer interruptions, code is currently Non-Blocking --> cool :)
+ *
  */
 
 #include <msp430.h>
@@ -20,7 +38,7 @@
 #include <uscia0_spi.h>
 #include <timers.h>
 
-#define PIXELS 3
+#define PIXELS 4
 
 
 void lightsOff(void);                                                   //Turns all LEDS OFF
@@ -33,11 +51,9 @@ void xmasTinsel (unsigned char LEDS[PIXELS][3]);
 void visual1(unsigned char LEDS[PIXELS][3]);
 void visual2(unsigned char LEDS[PIXELS][3]);
 
-unsigned char RXData =0;
-unsigned char TXData;
 
-//unsigned char LEDS[4][3] = {{0xFF,0x00,0x00},{0x00,0xFF,0x00},{0x00,0x00,0xFF},{0xFF,0xA5,0x00}};
-unsigned char LEDS[3][3] = {{0xFF,0x00,0x00},{0x00,0xFF,0x00},{0x00,0x00,0xFF}};
+unsigned char LEDS[4][3] = {{0xDC,0x14,0x3C},{0x00,0x64,0x00},{0x00,0xBF,0xFF},{0xFF,0xA5,0x00}};
+//unsigned char LEDS[3][3] = {{0xFF,0x00,0x00},{0x00,0xFF,0x00},{0x00,0x00,0xFF}};
 
 static unsigned char auxPixel[3];
 
@@ -51,59 +67,20 @@ int main(void)
   SPI_init();
   lightsOff();
 
- /* initTimerA0();
-  setTimerA0(TIMERA0_1SEC);
+  initTimerA0();
+  setTimerA0(TIMERA0_1SEC/4);
   startTimerA0();
-*/
+
+
   while(1){
 
       sendFrame(LEDS);
-
- /*     for(j=0 ; j<5;j++)
-          __delay_cycles(65535);
-      lightsOff();*/
-      for(j=0 ; j<15;j++)
-          __delay_cycles(65535);
-
       xmasTinsel(LEDS);
-     // __bis_SR_register(LPM0_bits + GIE);      // CPU off, enable interrupts
-  }
-/*
-  while(1)
-  {
-    UCA0TXBUF = TXData;
-    __bis_SR_register(LPM0_bits + GIE);      // CPU off, enable interrupts
-    __no_operation();                       // Remain in LPM0
-    __delay_cycles(20000);                   // Delay before next transmission
-  }
-*/
-}
 
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=USCI_A0_VECTOR
-__interrupt void USCI_A0_ISR(void)
-#elif defined(__GNUC__)
-void __attribute__ ((interrupt(USCI_A0_VECTOR))) USCI_A0_ISR (void)
-#else
-#error Compiler not supported!
-#endif
-{
-  volatile unsigned int i;
-
-  switch(__even_in_range(UCA0IV,0x04))
-  {
-    case 0: break;                          // Vector 0 - no interrupt
-    case 2:
-           RXData = UCA0RXBUF;
-           UCA0IFG &= ~UCRXIFG;
-           __bic_SR_register_on_exit(CPUOFF);// Wake up to setup next TX
-           break;
-    case 4:
-          UCA0TXBUF = TXData;             // Transmit characters
-          UCA0IE &= ~UCTXIE;
-          break;
-    default: break;
+      __bis_SR_register(LPM0_bits + GIE);      // CPU off, enable interrupts
+      //__no_operation();                       // Equivalent to an assembly NOP
   }
+
 }
 
 /*
@@ -142,17 +119,10 @@ void xmasTinsel (unsigned char LEDS[PIXELS][3]){
 
 
     int j;
-    unsigned char foo1;
-    unsigned char foo2;
-    unsigned char foo3;
-    /*
+
     auxPixel[0] = LEDS[0][0];
     auxPixel[1] = LEDS[0][1];
     auxPixel[2] = LEDS[0][2];
-*/
-    foo1 = LEDS[0][0];
-    foo2 = LEDS[0][1];
-    foo3 = LEDS[0][2];
 
     for(j=0 ; j<PIXELS-1 ; j++){
 
@@ -162,14 +132,9 @@ void xmasTinsel (unsigned char LEDS[PIXELS][3]){
 
     }
 
-  /*  LEDS[PIXELS][0] = auxPixel[0];
-    LEDS[PIXELS][1] = auxPixel[1];
-    LEDS[PIXELS][2] = auxPixel[2];
-    */
-    LEDS[PIXELS-1][0] = foo1;
-    LEDS[PIXELS-1][1] = foo2;
-    LEDS[PIXELS-1][2] = foo3;
-
+    LEDS[PIXELS-1][0] = auxPixel[0];
+    LEDS[PIXELS-1][1] = auxPixel[1];
+    LEDS[PIXELS-1][2] = auxPixel[2];
 
 }
 
