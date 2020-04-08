9b221b9e8d20d6b60ec72e90f9f10bbbfee456b4
gabriel-napal <gabrielnapalhm@gmail.com>
2020-04-09 00:05:59 +0200

    Added configuration of SMCLK for TimerA0 pursposes

--- clock.c
+++ clock.c
@@ -13,7 +13,7 @@ void CS_init (void){
     CSCTL0_H = 0xA5;                            //Power Up Clear
     CSCTL1 |= DCORSEL;                          // Sets Clock sources to maximum range
     CSCTL1 |= DCOFSEL0 + DCOFSEL1;              // Set max. DCO setting (24Mhz)
-    CSCTL2 = SELA__DCOCLK + SELS_3 + SELM_3;    // Set clock sources ACLK = SUBCLK = MCLK = DCO
-    CSCTL3 = DIVA__4 + DIVS_3 + DIVM_3;         // set ACLK to 6 Mhz, SUBCLK and MCLK to 4Mhz
+    CSCTL2 = SELA__DCOCLK + SELS__VLOCLK + SELM_3;    // Set clock sources ACLK; SUBCLK = VLOCLK;  MCLK = DCO
+    CSCTL3 = DIVA__4 + DIVS__1 + DIVM_3;         // set ACLK to 6 Mhz, SUBCLK to 8.42kHz and MCLK to 4Mhz
 
 }
