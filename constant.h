5088be33fbd6f537755e9119fcdc9221ec88f413
Laety-Nuf <laetitia.innocente@gmail.com>
2020-04-14 23:24:51 +0200

    Use to summarize constant

---
+++ constant.h
@@ -0,0 +1,23 @@
+/*
+ * constant.h
+ *
+ *  Created on: 14 avr. 2020
+ *      Author: Laety
+ */
+
+#ifndef CONSTANT_H_
+#define CONSTANT_H_
+
+
+#define HEIGHT  6
+#define LENGTH  5
+#define PIXELS  HEIGHT * LENGTH
+
+#define USER_NO_OPTION      0
+#define USER_OPTION_S1      0x01
+#define USER_OPTION_S2      0x02
+#define USER_OPTION_S3      0x04
+#define USER_OPTION_S4      0x08
+
+
+#endif /* CONSTANT_H_ */
