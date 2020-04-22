e28737878494ea9b7d99317b056f4473ce1f3a67
Laety-Nuf <laetitia.innocente@gmail.com>
2020-04-22 22:31:23 +0200

    First edition of tetris

--- easyGame.c
+++ easyGame.c
@@ -19,8 +19,9 @@
 #define DIRECTION_Y_DECREASING      3
 
 #define USER_NO_TURN      0
-#define USER_TURN_LEFT    1
-#define USER_TURN_RIGHT   2
+#define USER_GO_LEFT    1
+#define USER_GO_RIGHT   2
+#define USER_TURN_OBJECT   3
 
 #define GREEN_APPLE      0
 #define RED_APPLE    1
@@ -32,11 +33,12 @@ void game1(color_t LEDS[PIXELS]){
 
     unsigned int x;
     unsigned int y;
+    unsigned int y_delete;
     unsigned int i;
 
     unsigned char userOption[KEYBOARD_BUFFER] = {USER_NO_OPTION, USER_NO_OPTION, USER_NO_OPTION, USER_NO_OPTION, USER_NO_OPTION};
 
-    unsigned char exit = 0;
+    unsigned char exit = 0 ;
 
     //Initializes array
     for (x = 0 ; x < LENGTH ; x++){
@@ -162,10 +164,10 @@ void snake(color_t LEDS[PIXELS]){
             exit = 1;
             break;
         case USER_OPTION_S3: // To go right
-            turnCommand = USER_TURN_RIGHT;
+            turnCommand = USER_GO_RIGHT;
             break;
         case USER_OPTION_S4: // To go left
-            turnCommand = USER_TURN_LEFT;
+            turnCommand = USER_GO_LEFT;
             break;
         }
 
@@ -175,7 +177,7 @@ void snake(color_t LEDS[PIXELS]){
         }
         userOption[KEYBOARD_BUFFER-1] = USER_NO_OPTION;
 
-        if (turnCommand == USER_TURN_RIGHT) {
+        if (turnCommand == USER_GO_RIGHT) {
             switch (direction){
             case DIRECTION_X_INCREASING:
                 direction = DIRECTION_Y_DECREASING;
@@ -191,7 +193,7 @@ void snake(color_t LEDS[PIXELS]){
                 break;
             }
         }
-        else if (turnCommand == USER_TURN_LEFT) {
+        else if (turnCommand == USER_GO_LEFT) {
             switch (direction){
             case DIRECTION_X_INCREASING:
                 direction = DIRECTION_Y_INCREASING;
@@ -273,8 +275,8 @@ void snake(color_t LEDS[PIXELS]){
             testApple = 0;
             while (testApple == 0){
                 testApple = 1;
-                apple[0] = pseudoRandom(LENGTH);
-                apple[1] = pseudoRandom(HEIGHT);
+                apple[0] = rand_range16(LENGTH);
+                apple[1] = rand_range16(HEIGHT);
                 for (i = 0; i< snakeLength; i++){
                     if ((snake[i][0]== apple[0]) && (snake[i][1] == apple[1])){
                        testApple = 0;
@@ -283,7 +285,7 @@ void snake(color_t LEDS[PIXELS]){
                 }
             }
 
-            if (pseudoRandom(100)>75){
+            if (rand_range16(100)>75){
                 apple[2] = GREEN_APPLE;
                 LedTable [apple[0]][apple[1]] =  green_medium_1;
             }
@@ -299,6 +301,337 @@ void snake(color_t LEDS[PIXELS]){
             antiAliasGPIO(userOption, 5);
             turnCommand = USER_NO_TURN;
         }
+    }
+
+}
+
+/** KNOWN BUGS TO BE FIXED :
+> Turn object function is not available
+> There is no way to lose
+> Increasing speed
+ */
+
+void tetris(color_t LEDS[PIXELS]){
+
+    // templates of block
+    objectTetris o_block = {{LENGTH/2, HEIGHT - 1},{LENGTH/2 + 1, HEIGHT - 1},{LENGTH/2, HEIGHT},{LENGTH/2 + 1, HEIGHT},yellow_dark_1};
+    objectTetris i_block = {{LENGTH/2, HEIGHT - 1},{LENGTH/2, HEIGHT },{LENGTH/2, HEIGHT + 1},{LENGTH/2, HEIGHT + 2},cyan_bright_1};
+    objectTetris s_block = {{LENGTH/2, HEIGHT - 1},{LENGTH/2 + 1, HEIGHT - 1},{LENGTH/2 + 1, HEIGHT},{LENGTH/2 + 2, HEIGHT},green_dark_1};
+
+    objectTetris t_block = {{LENGTH/2, HEIGHT-1},{LENGTH/2 - 1, HEIGHT-1},{LENGTH/2, HEIGHT},{LENGTH/2 + 1, HEIGHT-1},purple_medium_1};
+    objectTetris j_block = {{LENGTH/2, HEIGHT},{LENGTH/2, HEIGHT -1},{LENGTH/2 - 1, HEIGHT - 1},{LENGTH/2, HEIGHT + 1},blue_dark_2};
+    objectTetris l_block = {{LENGTH/2, HEIGHT},{LENGTH/2 + 1, HEIGHT -1},{LENGTH/2, HEIGHT - 1},{LENGTH/2, HEIGHT + 1},orange_dark_1};
+
+    objectTetris z_block = {{LENGTH/2 + 1, HEIGHT - 1},{LENGTH/2 + 2, HEIGHT - 1},{LENGTH/2, HEIGHT},{LENGTH/2 + 1, HEIGHT},red_dark_3};
+
+    //setting the object that will be used
+    objectTetris block = {{0,0},{0,0},{0,0},{0,0},color_off};
+
+    // Define a matrix that contains the RGB color code for each pair (x,y)
+    color_t LedTable[LENGTH][HEIGHT];
+    unsigned char userOption[KEYBOARD_BUFFER] = {USER_NO_OPTION, USER_NO_OPTION, USER_NO_OPTION, USER_NO_OPTION, USER_NO_OPTION};
+    unsigned char exit = 0;
+    coordinates *tempPoint;
+
+    unsigned int x;
+    unsigned int y;
+    unsigned int y_delete;
+    unsigned int i;
+
+    // Boolean to be used for intern check
+    unsigned int newObject = TRUE;
+    unsigned int deleteLine = 0;
+    unsigned int checkNextBlock = FALSE;
+
+    // To be used for choosing the next object
+    unsigned int randomObject = 0;
+    unsigned int speed = 10;
+    unsigned int time_count = 0;
+
+    color_t background = color_off;
+
+    unsigned int turnCommand = USER_NO_TURN;
+
+    //Initializes array
+    for (x = 0 ; x < LENGTH ; x++){
+        for (y = 0 ; y < HEIGHT ; y++){
+            LedTable[x][y] = background;
+        }
+    }
+
+    while (exit!=1){
+        // Creates object
+        if (newObject == TRUE) {
+            randomObject = rand_range16(7);
+                   switch(randomObject){
+                       case 0:
+                           block = o_block;
+                           break;
+                       case 1:
+                           block = i_block;
+                           break;
+                       case 2:
+                           block = s_block;
+                           break;
+                       case 3:
+                           block = t_block;
+                           break;
+                       case 4:
+                           block = j_block;
+                           break;
+                       case 5:
+                           block = l_block;
+                           break;
+                       case 6:
+                           block = z_block;
+                           break;
+                   }
+
+
+                   // set the new object in the top of the game and check for each point, if it's included in the display
+                   if (block.point1.y < HEIGHT)
+                       LedTable[block.point1.x][block.point1.y] = block.color;
+                   if (block.point2.y < HEIGHT)
+                       LedTable[block.point2.x][block.point2.y] = block.color;
+                   if (block.point3.y < HEIGHT)
+                       LedTable[block.point3.x][block.point3.y] = block.color;
+                   if (block.point4.y < HEIGHT)
+                       LedTable[block.point4.x][block.point4.y] = block.color;
+
+                   newObject = FALSE;
+        }
+        else if (newObject == FALSE){
+                switch (userOption[0]){
+                case USER_OPTION_S1:
+                    //To turn the position of the object
+                    turnCommand = USER_TURN_OBJECT;
+                    break;
+                case USER_OPTION_S2:
+                    //exit effect / game
+                    exit = 1;
+                    break;
+                case USER_OPTION_S3: // To go right
+                    turnCommand = USER_GO_RIGHT;
+                    break;
+                case USER_OPTION_S4: // To go left
+                    turnCommand = USER_GO_LEFT;
+                    break;
+                }
+                // Erases the used option (shift left the buffer)
+                for (i = 0; i < KEYBOARD_BUFFER-1 ; i++){
+                    userOption[i]=userOption[i+1];
+                }
+                userOption[KEYBOARD_BUFFER-1] = USER_NO_OPTION;
+
+               if (turnCommand != USER_NO_OPTION){
+                   switch (turnCommand){
+                   case USER_GO_LEFT :
+                       if ((block.point1.x != 0) && (block.point2.x != 0) && (block.point3.x != 0) && (block.point4.x != 0)){  //Check if the block is not at the floor
+                           checkNextBlock = TRUE;
+                           tempPoint = &block.point1;
+                           for (i = 0; i < 4; i++){
+                               if ((*tempPoint).y < HEIGHT) {
+                                   if ((LedTable[(*tempPoint).x-1][(*tempPoint).y].R != background.R) || (LedTable[(*tempPoint).x-1][(*tempPoint).y].B != background.B) || (LedTable[(*tempPoint).x-1][(*tempPoint).y].G != background.G)) {
+                                       if (((((*tempPoint).x - 1) == block.point1.x) && (((*tempPoint).y) == block.point1.y)) || ((((*tempPoint).x - 1) == block.point2.x) && (((*tempPoint).y) == block.point2.y)) || ((((*tempPoint).x - 1) == block.point3.x) && (((*tempPoint).y) == block.point3.y)) ||  ((((*tempPoint).x - 1) == block.point4.x) && (((*tempPoint).y) == block.point4.y)))
+                                           checkNextBlock = checkNextBlock;
+                                       else {
+                                           checkNextBlock = FALSE;
+                                           break;
+                                       }
+                                   }
+                               }
+                               tempPoint = (++tempPoint);
+                           }
+                           if (checkNextBlock==TRUE){
+                               //Movement is valid, then erase the old block position
+                               moveTetrisObject(LedTable, &block, -1, 0, &background);
+                           }
+                       }
+                       break;
+                   case USER_GO_RIGHT :
+                       if ((block.point1.x < LENGTH - 1) && (block.point2.x < LENGTH - 1) && (block.point3.x < LENGTH - 1) && (block.point4.x < LENGTH - 1)) {
+                           checkNextBlock = TRUE;
+                           tempPoint = &block.point1;
+                           for (i = 0; i < 4; i++){
+                               if ((*tempPoint).y < HEIGHT) {
+                                   if ((LedTable[(*tempPoint).x + 1][(*tempPoint).y].R != background.R) || (LedTable[(*tempPoint).x + 1][(*tempPoint).y].B != background.B) || (LedTable[(*tempPoint).x + 1][(*tempPoint).y].G != background.G)) {
+                                       if (((((*tempPoint).x + 1) == block.point1.x) && (((*tempPoint).y) == block.point1.y)) || ((((*tempPoint).x + 1) == block.point2.x) && (((*tempPoint).y) == block.point2.y)) || ((((*tempPoint).x + 1) == block.point3.x) && (((*tempPoint).y) == block.point3.y)) ||  ((((*tempPoint).x + 1) == block.point4.x) && (((*tempPoint).y) == block.point4.y)))
+                                           checkNextBlock = checkNextBlock;
+                                       else {
+                                           checkNextBlock = FALSE;
+                                           break;
+                                       }
+                                   }
+                               }
+                               tempPoint = (++tempPoint);
+                           }
+                           if (checkNextBlock==TRUE){
+                               //Movement is valid, then erase the old block position
+                               moveTetrisObject(LedTable, &block, 1, 0, &background);
+                           }
+                       }
+                       break;
+                   case USER_TURN_OBJECT :
+                       RotateTetrisObject(&block, randomObject);
+                       break;
+                   }
+               }
+
+               if (time_count == speed){
+                   checkNextBlock = FALSE ;
+                   if ((block.point1.y !=0) && (block.point2.y !=0) && (block.point3.y !=0) && (block.point4.y !=0)){
+                       checkNextBlock = TRUE ;
+                       tempPoint = &block.point1;
+                       for (i = 0; i < 4; i++){
+                           if ((*tempPoint).y < HEIGHT) {
+                               if ((LedTable[(*tempPoint).x][(*tempPoint).y - 1].R != background.R) || (LedTable[(*tempPoint).x][(*tempPoint).y - 1].B != background.B) || (LedTable[(*tempPoint).x][(*tempPoint).y - 1].G != background.G)) {
+                                   if (((((*tempPoint).x) == block.point1.x) && (((*tempPoint).y - 1) == block.point1.y)) || ((((*tempPoint).x) == block.point2.x) && (((*tempPoint).y - 1) == block.point2.y)) || ((((*tempPoint).x) == block.point3.x) && (((*tempPoint).y - 1) == block.point3.y)) ||  ((((*tempPoint).x) == block.point4.x) && (((*tempPoint).y - 1) == block.point4.y)))
+                                       checkNextBlock = checkNextBlock;
+                                   else {
+                                       checkNextBlock = FALSE;
+                                       break;
+                                   }
+                               }
+                           }
+                           tempPoint = (++tempPoint);
+                       }
+                   }
+                   if (checkNextBlock==TRUE){
+                       //Movement is valid, then erase the old block position
+                       moveTetrisObject(LedTable, &block, 0, -1, &background);
+                   }
+                   else {
+                       for (y=0; y<HEIGHT; y++){
+                           deleteLine = TRUE;
+                           for (x=0; x<LENGTH; x++){
+                               if ((LedTable[x][y].R==background.R) && (LedTable[x][y].G==background.G) && (LedTable[x][y].B==background.B)){
+                                   deleteLine = FALSE;
+                                   break;
+                               }
+                           }
+                           if (deleteLine == TRUE){
+                               for (y_delete = y; y_delete<HEIGHT-1; y_delete++){
+                                   for (x=0; x<LENGTH; x++){
+                                       LedTable[x][y_delete].R = LedTable[x][y_delete+1].R;
+                                       LedTable[x][y_delete].G = LedTable[x][y_delete+1].G;
+                                       LedTable[x][y_delete].B = LedTable[x][y_delete+1].B;
+                                   }
+                               }
+                               for (x=0; x<LENGTH; x++){
+                                   LedTable[x][HEIGHT-1] = background;
+                               }
+                               y--;
+                           }
+                       }
+                       newObject = TRUE;
+                   }
+                   time_count = 0;
+               }
+        }
+
+        array2Vector(LedTable,LEDS);
+        sendFrame(LEDS);
+        antiAliasGPIO(userOption, 2);
+        time_count++;
+        turnCommand = USER_NO_TURN;
+
+    }
+
+}
+
+
+void moveTetrisObject(color_t LedTable[LENGTH][HEIGHT], objectTetris* block, signed int movX, signed int movY, color_t* background){
+
+    if ((*block).point1.y < HEIGHT)
+        LedTable[(*block).point1.x][(*block).point1.y] = *background;
+    if ((*block).point2.y < HEIGHT)
+        LedTable[(*block).point2.x][(*block).point2.y] = *background;
+    if ((*block).point3.y < HEIGHT)
+        LedTable[(*block).point3.x][(*block).point3.y] = *background;
+    if ((*block).point4.y < HEIGHT)
+        LedTable[(*block).point4.x][(*block).point4.y] = *background;
+
+    (*block).point1.x = (*block).point1.x + movX;
+    (*block).point2.x = (*block).point2.x + movX;
+    (*block).point3.x = (*block).point3.x + movX;
+    (*block).point4.x = (*block).point4.x + movX;
+    (*block).point1.y = (*block).point1.y + movY;
+    (*block).point2.y = (*block).point2.y + movY;
+    (*block).point3.y = (*block).point3.y + movY;
+    (*block).point4.y = (*block).point4.y + movY;
+
+    if ((*block).point1.y < HEIGHT)
+        LedTable[(*block).point1.x][(*block).point1.y] = (*block).color;
+    if ((*block).point2.y < HEIGHT)
+        LedTable[(*block).point2.x][(*block).point2.y] = (*block).color;
+    if ((*block).point3.y < HEIGHT)
+        LedTable[(*block).point3.x][(*block).point3.y] = (*block).color;
+    if ((*block).point4.y < HEIGHT)
+        LedTable[(*block).point4.x][(*block).point4.y] = (*block).color;
+}
+
+/*
+ * Performs a simple 2-D Matrix rotation:
+ *
+ * 1) Sets a first pivot-point
+ * 2) then calculates the coordinates relative to the pivot (tempX and tempY)
+ *
+ * 3) Rotates and sets the absolute coordinate in the same step
+ *      newX = tempY + pivot_x
+ *      newY = -tempX + pivot_y
+ *
+ */
+void RotateTetrisObject(objectTetris* block, int randomObject){
+
+    unsigned int i;
+    unsigned int canRotate;
+    int tempX;
+    int tempY;
+    int tempXforCheck;
+    int tempYforCheck;
+    coordinates *tempPoint;
+
+    // o_block
+    if  (randomObject == 0){
+        //Do nothing
+    }
+    // i_block
+    else if (randomObject == 1){
+
+    }
+    // anything else
+    else {
+        //Sets a pointer at point 2. Point 1 is the pivot, then it does not rotate
+        // First, only check if it can rotate
+        tempPoint = &((*block).point2);
+        canRotate = TRUE;
+        for (i = 0; i<3 ; i++) {
+            tempX = (*tempPoint).x - block->point1.x ;  //Obtains the relative coordinates
+            tempY = (*tempPoint).y - block->point1.y ;
+            tempXforCheck = tempY + block->point1.x ;  //Rotates and displaces to absolute coordinates
+            tempYforCheck = -tempX + block->point1.y ;
+            if (tempYforCheck < HEIGHT) {
+                if ((LedTable[tempXforCheck][tempYforCheck].R != background.R) || (LedTable[tempXforCheck][tempYforCheck].B != background.B) || (LedTable[tempXforCheck][tempYforCheck].G != background.G)) {
+                    if (((((*tempPoint).x) == block.point1.x) && (((*tempPoint).y - 1) == block.point1.y)) || ((((*tempPoint).x) == block.point2.x) && (((*tempPoint).y - 1) == block.point2.y)) || ((((*tempPoint).x) == block.point3.x) && (((*tempPoint).y - 1) == block.point3.y)) ||  ((((*tempPoint).x) == block.point4.x) && (((*tempPoint).y - 1) == block.point4.y)))
+                                                   checkNextBlock = checkNextBlock;
+                                               else {
+                                                   checkNextBlock = FALSE;
+                                                   break;
+                                               }
+                                           }
+                                       }
+            tempPoint = (++tempPoint);
+        }
+
+        tempPoint = &((*block).point2);
+                for (i = 0; i<3 ; i++) {
+                    tempX = (*tempPoint).x - block->point1.x ;  //Obtains the relative coordinates
+                    tempY = (*tempPoint).y - block->point1.y ;
+                    (*tempPoint).x = tempY + block->point1.x ;  //Rotates and displaces to absolute coordinates
+                    (*tempPoint).y = -tempX + block->point1.y ;
+                    tempPoint = (++tempPoint);
+                }
+
     }
 
 }
--- easyGame.h
+++ easyGame.h
@@ -10,8 +10,24 @@
 
 #include <COLORS.h>
 
+typedef struct coordinates {
+        unsigned int x;
+        unsigned int y;
+    } coordinates ;
+
+    typedef struct objectTetris{
+        coordinates point1;
+        coordinates point2;
+        coordinates point3;
+        coordinates point4;
+        color_t color;
+    } objectTetris;
+
 void game1(color_t LEDS[PIXELS]);
 void snake(color_t LEDS[PIXELS]);
+void tetris(color_t LEDS[PIXELS]);
+void moveTetrisObject(color_t LedTable[LENGTH][HEIGHT], objectTetris* block, signed int movX, signed int movY, color_t* background);
+void RotateTetrisObject(objectTetris* block, int randomObject);
 
 
 #endif /* EASYGAME_H_ */
